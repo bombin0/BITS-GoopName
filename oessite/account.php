c35bf3212814deb3acca433fa338964ac6a989f6
Marcus <s3821420@student.rmit.edu.au>
2020-10-04 15:52:42 +1100

    Create account.php

---
+++ oessite/account.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset = "utf-8">
+    <title>GroopName - Assignment 3</title>
+    <meta name = "author" content = "AR-TH-930-GroopName">
+    <meta name = "description" content = "AR-TH-930-GroopName Assignment 3 for Building IT Systems">
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+<body>
+<div class = "header">
+  <header>
+    <!--potential logo?-->
+    <!--
+      <nav class = "navbar">
+		  <a href = "#group ">The Team</a>
+		  <a href = "#description">Project Description</a>
+		  <a href = "#outcomes">Demonstrable Outcomes</a>
+		  <a href = "#justification">Project Justification</a>
+		  <a href = "#how">How</a>
+      <a href = "#timetable">Project Timetable</a>
+    </nav>
+    <-- Navbar (sit on top) -->
+<div class="w3-top">
+  <div class="w3-bar w3-white w3-card" id="myNavbar">
+    <img class="w3-bar-item w3-wide" src = "assets/img/OES.png" alt = "OES" style="width:8%">
+    <!-- Right-sided navbar links -->
+    <div class="w3-right w3-hide-small">
+      <a href="product.php" class="w3-bar-item w3-button">Products</a>
+      <a href="wishlist.php" class="w3-bar-item w3-button">Wishlist</a>
+      <a href="account.php" class="w3-bar-item w3-button">Account</a>
+    </div>
+  </header>
+</div>
+<div class = "pagecontain">
+<br><br><br><br>
+<h1>Account</h1>
+  <!-- login / logout feature -->
+
+</div>
+</body>
+
+<footer>
+    <p>&#169;2020 OES - Online Electronic Store</p>
+  </footer>
+</html>
